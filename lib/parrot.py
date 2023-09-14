7b528d113fd5133133dfd63da713e26716437f55
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-09-14 11:32:08 +0300

    Assignment done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,14 @@
-def parrot():
-    pass
+def parrot(argument=None):
+  if argument is None:
+   print("Squawk!")
+   return("Squawk!")
+  else:
+    print(argument)
+    return(argument)
+  
+parrot(6)
+    
+    
+
+
+   
